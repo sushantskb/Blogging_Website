c42e29f9faa574aedd0759bde3619c5c689fbc11
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-05-25 09:12:24 +0530

    created the compose route

--- controllers/blogging.controller.js
+++ controllers/blogging.controller.js
@@ -1,5 +1,5 @@
 exports.MainPage = (req, res)=>{
-    res.render('home', {startContent: homeStartingContent, items: post});
+    res.render('home', {startContent: homeStartingContent, items: posts});
 }
 
 exports.aboutPage = (req, res)=>{
@@ -9,3 +9,17 @@ exports.aboutPage = (req, res)=>{
 exports.contactPage = (req, res)=>{
     res.render('contact', {Contact: contactContent});
 };
+
+exports.composePage = (req, res)=>{
+    res.render('compose');
+};
+
+exports.composeActionPage = (req, res)=>{
+    let post = {
+        tittle : req.body.postTittle,
+        content : req.body.postBody
+    }
+    posts.push(post);
+    res.redirect("/");
+};
+
--- routes/router.js
+++ routes/router.js
@@ -6,5 +6,7 @@ const blogController = require('../controllers/blogging.controller');
 app.get("/", blogController.MainPage);
 app.get("/about", blogController.aboutPage);
 app.get("/contact", blogController.contactPage);
+app.get("/compose", blogController.composePage);
+app.post("/compose", blogController.composeActionPage);
 
 module.exports = app;
